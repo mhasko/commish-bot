894b3874f31ee5d593bda6baea43b601a3c4a48f
Mike Hasko <psychopez@gmail.com>
2019-09-13 20:40:08 -0400

    include division ref and pollmaster bot in permissions for created channels

--- commands/makematch.js
+++ commands/makematch.js
@@ -37,17 +37,19 @@ class MakeMatchCommand extends Commando.Command {
     }
 
     async run(message, args) {
-        let server = message.guild;
-        let commishBot = Helper.getRole(server, auth.user);
+        const server = message.guild;
+        const commishBot = Helper.getRole(server, auth.user);
+        const pollBot = Helper.getRole(server, 'Pollmaster');
+
 
         async function categoryCheck(category, options) {
             if(category){
                 // If this category doesn't exist, create it
                 if(!server.channels.find('name', category)){
-                     server.createChannel(category, "category").then(async newChannel => {
-                         options.parent = newChannel;
-                         return true;
-                     })
+                    server.createChannel(category, "category").then(async newChannel => {
+                        options.parent = newChannel;
+                        return true;
+                    })
                 } else {
                     options.parent = server.channels.find('name', category);
                 }
@@ -55,7 +57,8 @@ class MakeMatchCommand extends Commando.Command {
             return true;
         }
 
-        async function createChannelWith(blueTeam, redTeam, category, weekNum){
+        async function createChannelWith(blueTeam, redTeam, division, weekNum){
+            const divisionRefRole = Helper.getRole(server, division.divisionRole);
             let options = {
                 type: 'text',
                 permissionOverwrites: [
@@ -74,16 +77,24 @@ class MakeMatchCommand extends Commando.Command {
                     {
                         id: commishBot.id,
                         allow: ['VIEW_CHANNEL']
+                    },
+                    {
+                        id: pollBot.id,
+                        allow: ['VIEW_CHANNEL']
+                    },
+                    {
+                        id: divisionRefRole.id,
+                        allow: ['VIEW_CHANNEL']
                     }
                 ]
             };
             let prefixNum = '';
-            if(weekNum){prefixNum = `week ${weekNum}`};
+            if(weekNum){prefixNum = `week ${weekNum}`;}
             let newChannelName = `${prefixNum} ${blueTeam.name} vs ${redTeam.name}`;
             if (blueTeam && redTeam) {
-                await categoryCheck(category, options).then(
+                await categoryCheck(division.category, options).then(
                     server.createChannel(newChannelName, options).then(async newChannel => {
-                        newChannel.send(`${blueTeam} ${redTeam} This is the match channel.`);
+                        newChannel.send(`${blueTeam} ${redTeam} ${messages.newChannelMessage}`);
                         // newChannel.send(`${messages.newChannelMessage}`).then(sentMessage => {
                         //     sentMessage.react(consts.ReactionNumbers[0])
                         //         .then(() => sentMessage.react(consts.ReactionNumbers[1]))
@@ -105,106 +116,97 @@ class MakeMatchCommand extends Commando.Command {
             }
         }
 
-        // User provide the team roles, so let's just use them, assuming they are spelled correctly
-        if(args.length >= 2) {
-            let blueTeamRole = Helper.getRole(server, args[0]);
-            let redTeamRole = Helper.getRole(server, args[1]);
-            createChannelWith(blueTeamRole, redTeamRole, args[2]);
-        }
         // Proived a wizard to walk the user through all the options
-        else {
-            const optionFilter = (reaction, user) => {
-                return consts.ReactionNumbers.some((hex) => reaction.emoji.name === hex) && user.id === message.author.id;
-            };
+        const optionFilter = (reaction, user) => {
+            return consts.ReactionNumbers.some((hex) => reaction.emoji.name === hex) && user.id === message.author.id;
+        };
 
-            message.channel.send("What week is it?").then(weekMessage => {
-                weekMessage.react(consts.ReactionNumbers[1])
-                    .then(() => weekMessage.react(consts.ReactionNumbers[2]))
-                    .then(() => weekMessage.react(consts.ReactionNumbers[3]))
-                    .then(() => weekMessage.react(consts.ReactionNumbers[4]))
-                    .then(() => weekMessage.react(consts.ReactionNumbers[5]))
-                    .then(() => weekMessage.react(consts.ReactionNumbers[6]))
-                    .then(() => weekMessage.react(consts.ReactionNumbers[7]));
+        message.channel.send("What week is it?").then(weekMessage => {
+            weekMessage.react(consts.ReactionNumbers[1])
+                .then(() => weekMessage.react(consts.ReactionNumbers[2]))
+                .then(() => weekMessage.react(consts.ReactionNumbers[3]))
+                .then(() => weekMessage.react(consts.ReactionNumbers[4]))
+                .then(() => weekMessage.react(consts.ReactionNumbers[5]))
+                .then(() => weekMessage.react(consts.ReactionNumbers[6]))
+                .then(() => weekMessage.react(consts.ReactionNumbers[7]));
 
+            weekMessage.awaitReactions(optionFilter, {max: 1, time: 25000, errors: ['time']})
+                .then(weekNumCollected => {
+                    const weekNum = consts.ReactionNumbers.indexOf(weekNumCollected.first().emoji.name);
 
-                weekMessage.awaitReactions(optionFilter, {max: 1, time: 25000, errors: ['time']})
-                    .then(weekNumCollected => {
-                        const weekNum = consts.ReactionNumbers.indexOf(weekNumCollected.first().emoji.name);
-
-                        let divisionOptions = '';
-                        Object.keys(roles).forEach((key, index) => {
-                            divisionOptions += `${consts.ReactionNumbers[index+1]}: ${key}   `
-                        });
-                        message.channel.send(`${messages.wizard.whatDivision}\n${divisionOptions}`).then(divisionMessage => {
-                            divisionMessage.react(consts.ReactionNumbers[1])
-                                .then(() => divisionMessage.react(consts.ReactionNumbers[2]))
-                                .then(() => divisionMessage.react(consts.ReactionNumbers[3]))
-                                .then(() => divisionMessage.react(consts.ReactionNumbers[4]));
-
-                            divisionMessage.awaitReactions(optionFilter, {max: 1, time: 25000, errors: ['time']})
-                                .then(collected => {
-                                    let teamOptions = '';
-                                    const teamMap = {};
-                                    const reaction = collected.first();
-                                    const division = roles[consts.Divisions[reaction.emoji.name]];
-                                    Object.keys(division.teams).forEach((team, index) => {
-                                        teamOptions += `${consts.ReactionNumbers[index + 1]}: ${division.teams[team].name}\n`;
-                                        teamMap[consts.ReactionNumbers[index + 1]] = division.teams[team].discordRole;
-                                    });
-                                    message.channel.send(`${messages.wizard.whatHomeTeam}\n${teamOptions}`).then(homeTeamMessage => {
-                                        homeTeamMessage.react(consts.ReactionNumbers[1])
-                                            .then(() => homeTeamMessage.react(consts.ReactionNumbers[2]))
-                                            .then(() => homeTeamMessage.react(consts.ReactionNumbers[3]))
-                                            .then(() => homeTeamMessage.react(consts.ReactionNumbers[4]))
-                                            .then(() => homeTeamMessage.react(consts.ReactionNumbers[5]))
-                                            .then(() => homeTeamMessage.react(consts.ReactionNumbers[6]))
-                                            .then(() => {
-                                                if (Object.keys(division.teams).length === 7) {
-                                                    homeTeamMessage.react(consts.ReactionNumbers[7])
-                                                }
-                                            });
-
-                                        homeTeamMessage.awaitReactions(optionFilter, {
-                                            max: 1,
-                                            time: 25000,
-                                            errors: ['time']
-                                        })
-                                            .then(homeCollected => {
-                                                const reaction = homeCollected.first();
-                                                let blueTeamRole = Helper.getRole(server, teamMap[reaction.emoji.name]);
-                                                message.channel.send(`${messages.wizard.whatAwayTeam}\n${teamOptions}`).then(awayTeamMessage => {
-                                                    awayTeamMessage.react(consts.ReactionNumbers[1])
-                                                        .then(() => awayTeamMessage.react(consts.ReactionNumbers[2]))
-                                                        .then(() => awayTeamMessage.react(consts.ReactionNumbers[3]))
-                                                        .then(() => awayTeamMessage.react(consts.ReactionNumbers[4]))
-                                                        .then(() => awayTeamMessage.react(consts.ReactionNumbers[5]))
-                                                        .then(() => awayTeamMessage.react(consts.ReactionNumbers[6]))
-                                                        .then(() => {
-                                                            if (Object.keys(division.teams).length === 7) {
-                                                                awayTeamMessage.react(consts.ReactionNumbers[7])
-                                                            }
-                                                        });
-
-                                                    awayTeamMessage.awaitReactions(optionFilter, {
-                                                        max: 1,
-                                                        time: 25000,
-                                                        errors: ['time']
-                                                    })
-                                                        .then(awayCollected => {
-                                                            const reaction = awayCollected.first();
-                                                            let redTeamRole = Helper.getRole(server, teamMap[reaction.emoji.name]);
-                                                            createChannelWith(blueTeamRole, redTeamRole, division.category, weekNum);
-                                                        })
-                                                });
-                                            });
-                                    });
-
-                                });
-                        });
+                    let divisionOptions = '';
+                    Object.keys(roles).forEach((key, index) => {
+                        divisionOptions += `${consts.ReactionNumbers[index+1]}: ${key}   `
                     });
-            });
+                    message.channel.send(`${messages.wizard.whatDivision}\n${divisionOptions}`).then(divisionMessage => {
+                        divisionMessage.react(consts.ReactionNumbers[1])
+                            .then(() => divisionMessage.react(consts.ReactionNumbers[2]))
+                            .then(() => divisionMessage.react(consts.ReactionNumbers[3]))
+                            .then(() => divisionMessage.react(consts.ReactionNumbers[4]));
 
-        }
+                        divisionMessage.awaitReactions(optionFilter, {max: 1, time: 25000, errors: ['time']})
+                            .then(collected => {
+                                let teamOptions = '';
+                                const teamMap = {};
+                                const reaction = collected.first();
+                                const division = roles[consts.Divisions[reaction.emoji.name]];
+                                Object.keys(division.teams).forEach((team, index) => {
+                                    teamOptions += `${consts.ReactionNumbers[index + 1]}: ${division.teams[team].name}\n`;
+                                    teamMap[consts.ReactionNumbers[index + 1]] = division.teams[team].discordRole;
+                                });
+                                message.channel.send(`${messages.wizard.whatHomeTeam}\n${teamOptions}`).then(homeTeamMessage => {
+                                    homeTeamMessage.react(consts.ReactionNumbers[1])
+                                        .then(() => homeTeamMessage.react(consts.ReactionNumbers[2]))
+                                        .then(() => homeTeamMessage.react(consts.ReactionNumbers[3]))
+                                        .then(() => homeTeamMessage.react(consts.ReactionNumbers[4]))
+                                        .then(() => homeTeamMessage.react(consts.ReactionNumbers[5]))
+                                        .then(() => homeTeamMessage.react(consts.ReactionNumbers[6]))
+                                        .then(() => {
+                                            if (Object.keys(division.teams).length === 7) {
+                                                homeTeamMessage.react(consts.ReactionNumbers[7])
+                                            }
+                                        });
+
+                                    homeTeamMessage.awaitReactions(optionFilter, {
+                                        max: 1,
+                                        time: 25000,
+                                        errors: ['time']
+                                    })
+                                        .then(homeCollected => {
+                                            const reaction = homeCollected.first();
+                                            let blueTeamRole = Helper.getRole(server, teamMap[reaction.emoji.name]);
+                                            message.channel.send(`${messages.wizard.whatAwayTeam}\n${teamOptions}`).then(awayTeamMessage => {
+                                                awayTeamMessage.react(consts.ReactionNumbers[1])
+                                                    .then(() => awayTeamMessage.react(consts.ReactionNumbers[2]))
+                                                    .then(() => awayTeamMessage.react(consts.ReactionNumbers[3]))
+                                                    .then(() => awayTeamMessage.react(consts.ReactionNumbers[4]))
+                                                    .then(() => awayTeamMessage.react(consts.ReactionNumbers[5]))
+                                                    .then(() => awayTeamMessage.react(consts.ReactionNumbers[6]))
+                                                    .then(() => {
+                                                        if (Object.keys(division.teams).length === 7) {
+                                                            awayTeamMessage.react(consts.ReactionNumbers[7])
+                                                        }
+                                                    });
+
+                                                awayTeamMessage.awaitReactions(optionFilter, {
+                                                    max: 1,
+                                                    time: 25000,
+                                                    errors: ['time']
+                                                })
+                                                    .then(awayCollected => {
+                                                        const reaction = awayCollected.first();
+                                                        let redTeamRole = Helper.getRole(server, teamMap[reaction.emoji.name]);
+                                                        createChannelWith(blueTeamRole, redTeamRole, division, weekNum);
+                                                    })
+                                            });
+                                        });
+                                });
+
+                            });
+                    });
+                });
+        });
+
     }
 }
 
